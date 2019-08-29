d82d9e1b1ca1f50c0b98f7d934a2e6a53675bd42
Mark Ohnsman <mark.ohnsman@gmail.com>
2019-08-29 10:11:23 -0600

    Gen

--- app/Models/Spell.js
+++ app/Models/Spell.js
@@ -2,22 +2,21 @@ export default class Spell {
     constructor(data) {
         this.name = data.name
         this.level = data.level
-        this.description = data.description || data.desc.join("\n")
         this.range = data.range
         this.duration = data.duration
+        this.description = data.description || data.desc.join("\n")
         this.components = data.components
     }
 
     get Template() {
-        return this.title
+        return `
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">${this.name}</h5>
+                    <p class="card-text">${this.description}</p>
+                    <p>${this.level}<br>${this.range}<br>${this.duration}<br>${this.components}</p>
+                </div>
+            </div>`
     }
 }
 
-
-name: { type: String, required: true },
-description: { type: String, required: true },
-level: { type: Number },
-range: { type: String, required: true },
-duration: { type: String, required: true },
-components: [{ type: String }],
-    user: { type: String, required: true },
